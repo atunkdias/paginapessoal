c5741a39bcc17c59096227ee7e1e711cec380738
atunkdias <39276050+atunkdias@users.noreply.github.com>
2018-06-04 13:54:54 -0300

    navbar.php
    Barra de navegação...

---
+++ navbar.php
@@ -0,0 +1,11 @@
+<header id="header">
+            <div class="header-content clearfix"> <a class="logo" href="#">Arthur Dias</a>
+                <nav class="navigation" role="navigation">
+                    <ul class="primary-nav">
+                        <li><a href="#introduction"><b>Sobre mim</b></a></li>
+                        <li><a href="#works"><b>Últimos Trabalhos e coisas que eu gosto</b></a></li>
+                        <li><a href="#contact"><b>Contate-me</b></a></li>
+                    </ul>
+                </nav>
+                <a href="#" class="nav-toggle">Menu<span></span></a> </div>
+</header>
