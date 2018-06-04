3d2ee2cffb49b49935582c6aa33dfd6747816c3b
atunkdias <39276050+atunkdias@users.noreply.github.com>
2018-06-04 14:03:35 -0300

    Update links.php

--- links.php
+++ links.php
@@ -1,4 +1,4 @@
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Arthur Dias</title>
@@ -8,3 +8,9 @@
     <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="css/animate.min.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/retina.min.js"></script>
+    <script src="js/modernizr.js"></script>
+    <script src="js/main.js"></script>
