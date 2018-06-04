804b5bc127c7a7f6a03b074807d0df985a3b615e
atunkdias <39276050+atunkdias@users.noreply.github.com>
2018-06-04 14:02:16 -0300

    links
    links css, js e scripts

---
+++ links.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Arthur Dias</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
