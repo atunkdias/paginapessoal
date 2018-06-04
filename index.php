cfd8ce4eb1e92d794c261532acabb7b9ca7a3568
atunkdias <39276050+atunkdias@users.noreply.github.com>
2018-06-04 14:01:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,16 +7,9 @@
 <!--<![endif]-->
 
 <head>
-    <meta charset="utf-8">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Arthur Dias</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/jquery.fancybox.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/responsive.css">
-    <link rel="stylesheet" href="css/animate.min.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <?php
+    require 'links.php';
+    ?>
 </head>
 
 <body>
